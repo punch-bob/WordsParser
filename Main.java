775044650bfe41915d375bba5177192d83c2e58d
Konstantin Rybalko <rybalkokostyan@gmail.com>
2022-02-19 16:28:34 +0700

    Minor changes

--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
-import java.util.*;
-
 import src.*;
 
 public class Main 
 {
    public static void main(String[] args) 
    {
-        List<String> text = FileReciter.readFile("in.txt");
-        Set<Pair<String, Integer>> dictionary = DictionarySorter.sortDictionary(text);
-        CSVFileWriter.writeFile("out.csv", dictionary, text.size());
+        CSVParser.parseFile("in.txt", "out.csv");
    }
 }
