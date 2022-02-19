ca5fc147abe985bfd006acdfcbea098a8b0e7bc0
Konstantin Rybalko <rybalkokostyan@gmail.com>
2022-02-19 16:27:06 +0700

    Removed word count function

--- src/DictionarySorter.java
+++ src/DictionarySorter.java
@@ -5,10 +5,8 @@ import java.util.*;
 
 public class DictionarySorter 
 {    
-    public static TreeSet<Pair<String, Integer>> sortDictionary(List<String> listOfWords)
-    {
-        Set<Pair<String, Integer>> dictionary = new TreeSet<>();
-        dictionary = WordsCounter.countWordsFrequency(listOfWords);
+    public static TreeSet<Pair<String, Integer>> sortDictionary(Set<Pair<String, Integer>> dictionary)
+    {   
         TreeSet<Pair<String, Integer>> sortedDictionary = new TreeSet<>(new PairComparator());
         sortedDictionary.addAll(dictionary);
         return sortedDictionary;
